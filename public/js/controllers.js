fe74a8f66d525790fdcc6c50483cf4615f2e9465
jgerminario <jgerminario@gmail.com>
2015-02-08 15:53:36 -0800

    adds fix for the address data error on the form screen

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,16 +2,25 @@
 
 var appControllers = angular.module("appControllers", []);
 
-noPlaceLikeHome.controller("FormController", ["$scope", "$http", "$timeout",
-  function($scope, $http, $timeout){
+noPlaceLikeHome.controller("FormController", ["$scope", "$http", "$timeout", "$state",
+  function($scope, $http, $timeout, $state){
 
     $scope.formData = {}
+    $scope.error = "";
 
     $scope.submitAddressInfo = function(formData){
 
       $http.post('/api/offer', formData).success(function(response){
         console.log(response);
         $scope.recommendedOffer = response;
+        if(response.error){
+          $scope.error = "Address not found"
+        } else{
+          console.log("go");
+          $state.go("form.offer");
+        }
+          
+        
       })
 
     }
